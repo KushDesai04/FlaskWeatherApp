573262b0ca07705306c720725a6d0b9983c4dc86
Kush Desai <kush.desai@yahoo.co.nz>
2022-12-01 17:37:12 +1300

    cleanup of code

--- main.py
+++ main.py
@@ -1,36 +1,20 @@
-# Degrees website
+# Flask Weather App
 # Made by Kush Desai
-# From 3/5/2021 - 29/10/2021
-# License: MIT License
-
-'''
-Repo: https://github.com/KushDesai04/13DTP-Project
-'''
 
 
 from flask import Flask, render_template, request, url_for, redirect, abort, Response
 from config import Config
-# from forms import FilterForm
 import json
-import urllib
 import requests
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-
-# Sends info to every page but is used in nav
-@app.context_processor
-def context_processor():
-    return dict(unis='a')
-
-
 # Reroute when a 404 error is found
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
-
 
 # Home page
 @app.route('/')
@@ -65,17 +49,3 @@ def get_weather():
 
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'])
-
-
-
-'''
-params = {
-    #     'X-RapidAPI-Key': '48b40acd60mshe95879762d1b62ep1515b9jsn0c09828c305d',
-    #     'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-    # }
-    # r = requests.get(
-    # 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=christchurch',
-    # params=params)
-    # data = r.text
-    # print(json.loads(data))
-    # '''
